1a561373d553813ce36407d5eb6af67c153f4543
javadkavossi <jkavossi@gmail.com>
2024-12-06 14:00:00 +0100

    update Project

--- Project_Web_Api/src/api/validations/validations.go
+++ Project_Web_Api/src/api/validations/validations.go
@@ -9,12 +9,11 @@ import (
 
 func IranianMobileNumberValidator(fld validator.FieldLevel) bool {
 	value, ok := fld.Field().Interface().(string)
-
 	if !ok {
 		return false
 	}
-	result, err := regexp.MatchString(`^09(1[0-9]|2[0-2]|3[0-9]|9[0-9])[0-9]{7}$`, value)
 
+	result, err := regexp.MatchString(`^09(1[0-9]|2[0-2]|3[0-9]|9[0-9])[0-9]{7}$`, value)
 	if err != nil {
 		log.Println(err.Error())
 	}
