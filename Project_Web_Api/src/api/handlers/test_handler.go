af0daeacebfa78b9e85263310546851f4efd2372
javadkavossi <jkavossi@gmail.com>
2024-12-08 18:38:50 +0330

    update swgger

--- Project_Web_Api/src/api/handlers/test_handler.go
+++ Project_Web_Api/src/api/handlers/test_handler.go
@@ -112,7 +112,7 @@ func (h *TestHandler) Accounts(ctx *gin.Context) {
 // @Param userId header string false "User Id"
 // @Param contentType header string false "Content-Type"
 // @Failure 400 {object} helper.BaseHttpResponse "Bad request"
-// @Router /v1/test/user/HeaderBinder1 [Post]
+// @Router /v1/test/HeaderBinder1 [Post]
 func (h *TestHandler) HeaderBinder1(ctx *gin.Context) {
 	userId := ctx.GetHeader("userId")
 	contentType := ctx.GetHeader("Content-Type")
@@ -122,48 +122,93 @@ func (h *TestHandler) HeaderBinder1(ctx *gin.Context) {
 	}, true, http.StatusOK))
 }
 
-
-
-
+// HealthCheck godoc
+// @Summary Test Handler HeaderBinder2
+// @Description Test HeaderBinder1 Get Header Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param contentType header &header false "header Data struct"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/HeaderBinder2 [Post]
 func (h *TestHandler) HeaderBinder2(ctx *gin.Context) {
 	header := header{}
 	ctx.BindHeader(&header)
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(map[string]interface{}{
 		"result": "HeaderBinder2",
 		"Header": header,
-	})
+	}, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler QueryBinder1
+// @Description Test QueryBinder1 Get Query Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param name query string false "Name"
+// @Param family query string false "Family"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/QueryBinder1 [Post]
 func (h *TestHandler) QueryBinder1(ctx *gin.Context) {
 	name := ctx.Query("name")
 	family := ctx.Query("family")
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(map[string]string{
 		"result": "QueryBinder 1",
 		"name":   name,
 		"family": family,
-	})
+	}, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler QueryBinder2
+// @Description Test QueryBinder2 Get Query Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param name query string false "Name"
+// @Param family query string false "Family"
+// @Param id query array false "ID"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/QueryBinder2 [Post]
 func (h *TestHandler) QueryBinder2(ctx *gin.Context) {
 	ids := ctx.QueryArray("id")
 	name := ctx.Query("name")
 	family := ctx.Query("family")
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(map[string]interface{}{
 		"result": "QueryBinder 2",
 		"name":   name,
 		"family": family,
 		"ids":    ids,
-	})
+	}, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler UriBinder
+// @Description Test UriBinder Post Uri Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param Data path string false "Data"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/UriBinder/:Data [Post]
 func (h *TestHandler) UriBinder(ctx *gin.Context) {
 	data := ctx.Param("Data")
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(map[string]string{
 		"result":   "User Name",
 		"UserName": data,
-	})
+	}, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler BodyBinder
+// @Description Test BodyBinder Post Body Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param Data body personData false "Data"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/BodyBinder [Post]
 func (h *TestHandler) BodyBinder(ctx *gin.Context) {
 	person := personData{}
 	err := ctx.ShouldBindJSON(&person)
@@ -171,14 +216,18 @@ func (h *TestHandler) BodyBinder(ctx *gin.Context) {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, helper.GenerateBaseResponseWithValidationError("Bad Request !", false, http.StatusBadRequest, err))
 		return
 	}
-	// result := map[any]any{
-	// 	"result":     "BodyBinder",
-	// 	"personData": person,
-	// }
-
 	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(person, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler FormBinder
+// @Description Test FormBinder Post Body Data
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param Data body personData false "Data"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/FormBinder [Post]
 func (h *TestHandler) FormBinder(ctx *gin.Context) {
 	person := personData{}
 	err := ctx.ShouldBind(&person)
@@ -189,17 +238,24 @@ func (h *TestHandler) FormBinder(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(person, true, http.StatusOK))
 }
 
+// HealthCheck godoc
+// @Summary Test Handler FileBinder
+// @Description Test FileBinder Post File Data for Upload
+// @Tags Test
+// @Accept json
+// @produces json
+// @Param File formData true "File"
+// @Failure 400 {object} helper.BaseHttpResponse "Bad request"
+// @Router /v1/test/FileBinder [Post]
 func (h *TestHandler) FileBinder(ctx *gin.Context) {
 	file, _ := ctx.FormFile("file")
 	err := ctx.SaveUploadedFile(file, "file.png")
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, helper.GenerateBaseResponseWithValidationError("Bad Request !", false, http.StatusBadRequest, err))
 		return
 	}
-	ctx.JSON(http.StatusOK, gin.H{
-		"result":   "BodyBinder",
+	ctx.JSON(http.StatusOK, helper.GenerateBaseResponse(map[string]string{
+		"result":   "FileBinder",
 		"FileName": file.Filename,
-	})
+	}, true, http.StatusOK))
 }
