a97c1cef41de6d2d48987fbfc8ec3fbc41fc3e79
javadkavossi <jkavossi@gmail.com>
2024-12-25 14:00:00 +0100

    update Project

--- Project_Web_Api/src/api/routers/test_router.go
+++ Project_Web_Api/src/api/routers/test_router.go
@@ -12,13 +12,22 @@ func TestRouter(router *gin.RouterGroup) {
 	router.GET("/user/:id", h.UserById)
 	router.GET("/user/get-user-by-username/:username", h.UserName)
 	router.GET("/user/:id/accounts", h.Accounts)
+	
 	router.POST("/HeaderBinder1", h.HeaderBinder1)
+
 	router.POST("/HeaderBinder2", h.HeaderBinder2)
+
 	router.POST("/QueryBinder1", h.QueryBinder1)
+
 	router.POST("/QueryBinder2", h.QueryBinder2)
+
 	router.POST("/UriBinder/:Data", h.UriBinder)
+
 	router.POST("/BodyBinder", h.BodyBinder)
+
 	router.POST("/FormBinder", h.FormBinder)
+
 	router.POST("/file-binder", h.FileBinder)
+
 	router.POST("/add-user", h.AddUser)
 }
